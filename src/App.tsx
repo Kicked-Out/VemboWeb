b6b74aa2beda5014301ed14fdad837b36a1b3ab0
MrLazYT <swimervik@gmail.com>
2025-07-02 20:27:59 +0300

    implemented header

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,22 @@ import Home from "./pages/Home";
 import Lesson from "./pages/Lesson";
 import NotFound from "./pages/NotFound";
 import NavBar from "./components/navigation/navBar";
+import Header from "./components/header/header";
 
 function App() {
     return (
         <div className="flex">
             <NavBar />
 
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/lesson" element={<Lesson />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
+            <div className="content">
+                <Header />
+
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/lesson" element={<Lesson />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </div>
         </div>
     );
 }
