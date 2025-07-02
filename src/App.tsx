243d13569f2fb2776d612a413aa224a364f2eed4
MrLazYT <swimervik@gmail.com>
2025-07-02 18:58:59 +0300

    implemented navigation panel

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import "./styles/styles.css";
 import Home from "./pages/Home";
 import Lesson from "./pages/Lesson";
 import NotFound from "./pages/NotFound";
+import NavBar from "./components/navigation/navBar";
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/lesson" element={<Lesson />} />
-            <Route path="*" element={<NotFound />} />
-        </Routes>
+        <div className="flex">
+            <NavBar />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/lesson" element={<Lesson />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </div>
     );
 }
 
