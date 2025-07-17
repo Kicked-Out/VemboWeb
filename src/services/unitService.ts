60e15e0cfb4ff54b551ed6ef47158ff49059bf26
MrLa3YT <swimervik@gmail.com>
2025-07-17 21:56:34 +0300

    implemented ItemFetcher. Cleaned Code, modified home page

--- src/services/unitService.ts
+++ src/services/unitService.ts
@@ -1,41 +1,18 @@
 import type { UnitDTO } from "../DTOs/unitDTO";
+import ItemFetcher from "../helpers/itemFetcher";
 
 export class UnitService {
     private static baseUrl: string = "/api";
 
     public static async getAll(): Promise<UnitDTO[]> {
-        try {
-            const response = await fetch(`${this.baseUrl}/unit/getAll.json`);
+        const data = await ItemFetcher.fetchItems(`${this.baseUrl}/unit/getAll.json`);
 
-            if (response.ok) {
-                const data: UnitDTO[] = await response.json();
-
-                return data;
-            }
-
-            return [];
-        } catch (error) {
-            console.error("Fetch error:", error);
-
-            return [];
-        }
+        return data;
     }
 
     public static async getById(id: number): Promise<UnitDTO | null> {
-        try {
-            const response = await fetch(`${this.baseUrl}/unit/get/${id}.json`);
+        const data = await ItemFetcher.fetchItem(`${this.baseUrl}/unit/get/${id}.json`);
 
-            if (response.ok) {
-                const data: UnitDTO = await response.json();
-
-                return data;
-            }
-
-            return null;
-        } catch (error) {
-            console.error("Fetch error:", error);
-
-            return null;
-        }
+        return data;
     }
 }
